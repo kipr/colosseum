549cba96fdbf4ca167f2850c8f78e16acab641e2
twells46 <173561638+twells46@users.noreply.github.com>
2026-09-02 16:47:24 +0000

    Improved error checking

--- src/shared/scoreFormulaEval.ts
+++ src/shared/scoreFormulaEval.ts
@@ -1,12 +1,10 @@
-import { usesLegacyRawScoreFormula } from "../server/services/rawScoreFormula";
-
 const OPS = new Set(['+', '*', '<', '>', '===', '||'] as const);
 const OPS_SINGLE = '*+<>=|?:';
 type Op = typeof OPS extends Set<infer T> ? T : never;
 function isOp(value: string): value is Op {
   return OPS.has(value as Op);
 }
-export type Single = 
+export type Single =
   | { type: 'number'; value: number }
   | { type: 'string'; value: string }
   | { type: 'variable'; name: string };
@@ -25,24 +23,56 @@ export type Compound =
     };
 
 export type Expr = Single | Compound;
-export type ScoreFormulaEvalErr = string;
-export const DEPTH_EXCEEDED: ScoreFormulaEvalErr = 'Recursive depth exceeded';
-export const INVALID_OP: ScoreFormulaEvalErr = 'Invalid operation in formula';
+export type FormulaParseResult =
+  | {
+      ok: true;
+      expr: Expr;
+    }
+  | {
+      ok: false;
+      error: string;
+    };
+export const DEPTH_EXCEEDED = 'Recursive depth exceeded';
+export const LEN_EXCEEDED = 'Length exceeded';
+export const INVALID_OP = 'Invalid operation in formula';
+export const MISMATCHED_PAREN = 'Mismatched parenthesis';
 
-export function splitRec (strExp: string, recDepth: number): Expr | ScoreFormulaEvalErr {
+export function parseFormula(formula: string): FormulaParseResult {
+  if (formula.length > 1024) {
+    return {
+      ok: false,
+      error: LEN_EXCEEDED,
+    };
+  }
+  return splitRec(formula, 0);
+}
+
+function ok(expr: Expr): FormulaParseResult {
+  return {
+    ok: true,
+    expr: expr,
+  };
+}
+function err(error: string): FormulaParseResult {
+  return {
+    ok: false,
+    error: error,
+  };
+}
+
+function splitRec(strExp: string, recDepth: number): FormulaParseResult {
   // Base case: strExp is a single
   if (/^'\w*'$/.test(strExp)) {
-    return {type: "string", value: strExp.slice(1, -1)};
+    return ok({ type: 'string', value: strExp.slice(1, -1) });
   } else if (/^[a-z]\w*$/.test(strExp)) {
-    return {type: "variable", name: strExp};
+    return ok({ type: 'variable', name: strExp });
   } else if (/^[0-9.]*$/.test(strExp)) {
-    return {type: "number", value: parseInt(strExp, 10)}
+    return ok({ type: 'number', value: parseInt(strExp, 10) });
   }
 
-  if (recDepth > 256) {
-    return DEPTH_EXCEEDED;
+  if (recDepth > 64) {
+    return err(DEPTH_EXCEEDED);
   }
-
   // Find the position of the next operator
   let depth = 0;
   let minDepth = Number.MAX_SAFE_INTEGER;
@@ -67,6 +97,9 @@ export function splitRec (strExp: string, recDepth: number): Expr | ScoreFormula
       prec = 0;
     }
   }
+  if (depth !== 0) {
+    return err(MISMATCHED_PAREN);
+  }
   // Clean up extraneous parenthesis
   strExp = strExp.slice(minDepth, strExp.length - minDepth);
   split = split - minDepth;
@@ -85,39 +118,38 @@ export function splitRec (strExp: string, recDepth: number): Expr | ScoreFormula
       isTern = true;
   }
   if (isTern) {
-    const con = strExp.slice(0, tern_start-1).trim();
+    const con = strExp.slice(0, tern_start - 1).trim();
     const the = strExp.slice(tern_start + opLen, split).trim();
     const oth = strExp.slice(split + opLen).trim();
     const cons = splitRec(con, recDepth + 1);
+    if (!cons.ok) return cons;
     const thes = splitRec(the, recDepth + 1);
+    if (!thes.ok) return thes;
     const oths = splitRec(oth, recDepth + 1);
-    if (typeof cons === 'string' || typeof thes === 'string' || typeof oths === 'string') {
-      return cons;
-    }
+    if (!oths.ok) return oths;
 
-    return {
+    return ok({
       type: 'conditional',
-      condition: cons,
-      then: thes,
-      otherwise: oths
-    }
+      condition: cons.expr,
+      then: thes.expr,
+      otherwise: oths.expr,
+    });
   }
   const left = strExp.slice(0, split - opLen).trim();
-  const right = strExp.slice(split+1).trim();
-  const op = strExp.slice(split - opLen, split+1).trim();
+  const right = strExp.slice(split + 1).trim();
+  const op = strExp.slice(split - opLen, split + 1).trim();
   if (!isOp(op)) {
-    return INVALID_OP;
+    return err(INVALID_OP);
   }
   const leftSplit = splitRec(left, recDepth + 1);
+  if (!leftSplit.ok) return leftSplit;
   const rightSplit = splitRec(right, recDepth + 1);
-  if (typeof leftSplit === 'string' || typeof rightSplit === 'string') {
-    return leftSplit;
-  }
+  if (!rightSplit.ok) return rightSplit;
 
-  return {
+  return ok({
     type: 'binary',
     op: op as Op,
-    left: leftSplit,
-    right: rightSplit
-  }
+    left: leftSplit.expr,
+    right: rightSplit.expr,
+  });
 }
